2bd5d7092728cc3e33d5c7a3b89cb3f36ae90ce9
jak010 <bluetoon@naver.com>
2024-02-01 19:03:12 +0900

    add, coupon_service

--- coupons/src/repository/coupon_repository.py
+++ coupons/src/repository/coupon_repository.py
@@ -1,7 +1,12 @@
+from coupons.config.settings import db_session
+
+
 class CouponRepository:
 
     def __init__(self):
-        self.session = None
+        self.session = db_session()
 
     def find_by_id(self, coupon_id: int):
-        return True
+
+        self.session.query()
+
