826911eaf2ea0e8e56f515d2763d05ff393bff12
jak010 <bluetoon@naver.com>
2024-03-28 04:31:28 +0900

    update, redis pubsub with fastapi websocket

--- demonstration_apis/redis_pubsub_chat_api/src/controller2.py
+++ demonstration_apis/redis_pubsub_chat_api/src/controller2.py
@@ -1,20 +1,37 @@
 import asyncio
+import dataclasses
+import datetime
 import json
 import random
+import time
+import uuid
+from typing import Optional
 
-from fastapi import WebSocket, Depends, Path, WebSocketDisconnect, Body, UploadFile
+from fastapi import WebSocket, Path, WebSocketDisconnect, Body, UploadFile
 from fastapi.params import File
 from fastapi.responses import JSONResponse
 from fastapi.routing import APIRouter
+from pydantic import BaseModel
 
-from src.utils import ConnectionManager, AsyncRedisClient
+from src.service import RedisChannelManager
+from src.utils import AsyncRedisClient
 
 websocket_router2 = APIRouter(tags=['API'], prefix="/v2")
 
 async_redis_client = AsyncRedisClient()
 
-from pydantic import BaseModel
-import time
+
+@dataclasses.dataclass
+class Message:
+    message_id: str
+    sender: str
+    receiver: Optional[str]
+    content: str
+    timestamp: str
+
+    def to_json(self):
+        return json.dumps(dataclasses.asdict(self), ensure_ascii=False)
+        # return dataclasses.asdict(self)
 
 
 class Item(BaseModel):
@@ -25,21 +42,35 @@ class ItemFile(BaseModel):
     file: str
 
 
+from src.service import RedisChannelHistory, RedisChannelMessenger
+
+
 @websocket_router2.post("/channel/{channel_id}/send-message")
 async def send_message(
         channel_id: str = Path(),
         message: Item = Body(default=None)
 ):
-    user_id = random.randint(1, 2)
-    messages = json.dumps({
-        "timestamp": time.time(),
-        "sender": user_id,
-        "message": message.text,
-        "read_count": 1
-    }, ensure_ascii=False)
+    log = RedisChannelHistory(channel_id=channel_id)
+    live_chat = RedisChannelMessenger(channel_id=channel_id)
 
-    await async_redis_client.client.publish(channel_id, messages)
-    await async_redis_client.client.rpush(channel_id, messages)
+    user_id = "1"  # 고정된 user_id가 message 전송
+    message = {
+        "timestamp": datetime.datetime.now().isoformat(),
+        "sender": str(user_id),
+        "message": message.text,
+        "receiver": None
+    }
+
+    message = Message(
+        message_id=str(uuid.uuid4()),
+        sender=str(user_id),
+        receiver=None,
+        content="이상무",
+        timestamp=datetime.datetime.now().isoformat()
+    ).to_json()
+
+    await live_chat.send_message(message)
+    await log.add_log(message)
 
     return JSONResponse(status_code=200, content={})
 
@@ -54,7 +85,7 @@ async def send_file(
     to_base64 = base64.b64encode(await file.read())
     user_id = random.randint(1, 10000)
     messages = json.dumps({
-        "timestamp": time.time(),
+        "timestamp": datetime.datetime.now(),
         "sender": user_id,
         "file": to_base64.decode()
     })
@@ -68,38 +99,58 @@ async def send_file(
 
 async def receive_text_with_timeout(websocket, timeout):
     try:
-        task = asyncio.ensure_future(websocket.receive_text())
+        task = asyncio.ensure_future(websocket.receive_json())
         return await asyncio.wait_for(task, timeout=timeout)
     except asyncio.TimeoutError:
         return None
 
 
-@websocket_router2.websocket("/channel/{channel_id}")
+@websocket_router2.websocket("/channel/{channel_id}/{user_id}")
 async def enter_room(
         websocket: WebSocket,
-        manager: ConnectionManager = Depends(ConnectionManager),
         channel_id: str = Path(),
+        user_id: str = Path()
 ):
-    await manager.connect(websocket, channel_id=channel_id)
-    pubsub = async_redis_client.client.pubsub()
-    await pubsub.subscribe(channel_id)
+    RedisChannelManager(channel_id).add_user(user_id=user_id, user_data={"joined_time": time.time()})
+
+    redis_channel_history = RedisChannelHistory(channel_id=channel_id)
+    await websocket.accept()
+
+    # 1. Connection이 성공하면 subscribe 시키기
+    connection = RedisChannelMessenger(channel_id=channel_id)
+
+    # 2. chatting 읽음 표시
+    await redis_channel_history.mark_chat_read(user_id=user_id)
+
+    # 3. subscribe 이후 최근 채팅 로그 보내주기
+    datas = await redis_channel_history.read_latest_history_by_size(size=10)
+    await websocket.send_json(datas)
 
     try:
-        # Connection 성공 시, Log 보내기
-        messages = await async_redis_client.client.lrange(channel_id, -5, -1)
-        await websocket.send_text(
-            data=json.dumps(
-                [str(message) for message in messages]
-            ))
-
         while True:
             data = await receive_text_with_timeout(websocket, 0.01)
             if data is not None:
-                await websocket.send_text(data='received')
-            else:
-                if message := await pubsub.get_message(ignore_subscribe_messages=True):
-                    print(message)
-                    await websocket.send_text(str(message['data']))
+                if data.get('method') is None:
+                    await websocket.send_text(data='')
 
-    except WebSocketDisconnect:
-        manager.disconnect(websocket, channel_id=channel_id)
+                elif data['method'] == 'unread':
+                    # temp: 2번 유저가 안 읽은 메세지 수 보내주기
+                    unread_count = await redis_channel_history.fetched_by_unread_message_with_user(user_id=user_id)
+                    await websocket.send_text(data=json.dumps({"user": "2", "unread": unread_count}))
+
+                elif data['method'] == 'length':
+                    # chatting history의 수
+                    message_length = await redis_channel_history.read_messages_length()
+                    await websocket.send_text(data=json.dumps({"length": message_length}))
+
+                elif data['method'] == 'read_more':
+                    # chatting history의 수
+                    channel_histories = await redis_channel_history.read_latest_history_from_cursor(10)
+                    await websocket.send_json(data=channel_histories)
+
+
+            else:
+                if data := await connection.listen():
+                    await websocket.send_json(json.loads(data['data']))
+    except WebSocketDisconnect as e:
+        RedisChannelManager(channel_id).delete_user(user_id=user_id)
--- demonstration_apis/redis_pubsub_chat_api/src/service.py
+++ demonstration_apis/redis_pubsub_chat_api/src/service.py
@@ -1,11 +1,125 @@
-from utils import RedisClient
+import json
+
+from redis.client import PubSub
+
+from src.utils import RedisClient, AsyncRedisClient
 
 
-class ChatService:
+class RedisChannelManager:
     redis_client = RedisClient()
 
-    def enter_chant_room(self, chat_room_name: str):
-        print(chat_room_name)
+    def __init__(self, channe_id: str):
+        self.channel = f"channel:{channe_id}"
 
-    def on_message(self, message: str):
-        print(message)
+    def add_user(self, user_id: str, user_data: dict):
+        self.redis_client.client.hset(
+            name=self.channel,
+            key=f"user_{user_id}",
+            value=json.dumps(user_data, ensure_ascii=False)
+        )
+
+    def get_user(self, channel: str, user_id: str):
+        self.redis_client.client.hget(name=channel, key=user_id)
+
+    def get_chanels(self, channel_id: str):
+        self.redis_client.client.hgetall(name=channel_id)
+
+    def delete_user(self, user_id: str):
+        user = f"user_{user_id}"
+        self.redis_client.client.hdel(self.channel, user)
+
+
+class RedisChannelHistory:
+    async_redis_client = AsyncRedisClient()
+
+    def __init__(self, channel_id: str):
+        self.channel = f"channel_{channel_id}"
+
+        self._init_cursor_start = -5
+        self._init_cursor_end = -1
+
+    async def mark_chat_read(self, user_id):
+        logs = await self.async_redis_client.client.lrange(
+            name=self.channel,
+            start=-0,
+            end=-1
+        )
+        for idx, log in enumerate(logs):
+            payload = json.loads(log)
+
+            if payload['sender'] != user_id:
+                payload['receiver'] = user_id
+
+            await self.async_redis_client.client.lset(
+                name=self.channel,
+                index=idx,
+                value=json.dumps(payload, ensure_ascii=False)
+            )
+
+    async def read_latest_history_by_size(self, size: int) -> list[dict]:
+        channel_history = await self.async_redis_client.client.lrange(
+            name=self.channel,
+            start=-size,
+            end=-1
+        )
+        return sorted([json.loads(history) for history in channel_history], key=lambda x: x['timestamp'])
+
+    async def read_latest_history_from_cursor(self, cursor):
+        history_size = await self.read_messages_length()
+
+        if abs(self._init_cursor_end) > abs(history_size):
+            channel_history = await self.async_redis_client.client.lrange(name=self.channel, start=0, end=1)
+            return sorted([json.loads(history) for history in channel_history], key=lambda x: x['timestamp'])
+
+        channel_history = await self.async_redis_client.client.lrange(
+            name=self.channel,
+            start=self._init_cursor_start - cursor,
+            end=-self._init_cursor_end - cursor
+        )
+
+        self._init_cursor_start -= cursor
+        if self._init_cursor_end == -1:
+            self._init_cursor_end -= cursor - 1
+        else:
+            self._init_cursor_end -= cursor
+
+        return sorted([json.loads(history) for history in channel_history], key=lambda x: x['timestamp'])
+
+    async def read_messages_length(self) -> int:
+        return await self.async_redis_client.client.llen(name=self.channel)
+
+    async def fetched_by_unread_message_with_user(self, user_id):
+        channel_histories = await self.async_redis_client.client.lrange(
+            name=self.channel, start=0, end=-1
+        )
+        unread_count = 0
+        for channel_history in channel_histories:
+            data = json.loads(channel_history)
+            if data['sender'] == str(user_id) and data['receiver'] is None:
+                unread_count += 1
+        return unread_count
+
+    async def add_log(self, message_payload: str):
+        await self.async_redis_client.client.rpush(
+            self.channel,
+            message_payload
+        )
+
+
+class RedisChannelMessenger:
+    async_redis_client = AsyncRedisClient()
+
+    def __init__(self, channel_id: str):
+        self.channel = f"channel_{channel_id}"
+
+        self.connection: PubSub = self.async_redis_client.client.pubsub()
+
+    async def send_message(self, message_payload: str):
+        await self.async_redis_client.client.publish(
+            self.channel,
+            message=message_payload
+        )
+
+    async def listen(self):
+        await self.connection.subscribe(self.channel)
+        return await self.connection.get_message(ignore_subscribe_messages=True)
