1affb3ecfdab2f19f467dceb2c65086d9c09b8ed
jak010 <bluetoon@naver.com>
2024-04-28 13:20:26 +0900

    feature, pytest-bdd with member create test

---
+++ demonstration_apis/movie_application_api/test/features/steps/test_member.py
@@ -0,0 +1,16 @@
+from behave import *
+
+
+@given('한글한글한글')
+def step_impl(context):
+    pass
+
+
+@when('we implement a test')
+def step_impl(context):
+    assert True is not False
+
+
+@then('behave will test it for us!')
+def step_impl(context):
+    assert context.failed is False
--- demonstration_apis/movie_application_api/test/test_member/test_create_member.py
+++ demonstration_apis/movie_application_api/test/test_member/test_create_member.py
@@ -1,26 +1,26 @@
-from pytest_bdd import given, when, then, scenario
-
-from src.member import MemberEntity
-
-
-@scenario("../features/member/member_create.feature", "사용자는 계정을 만들 수 있습니다.")
-def test_create_member():
-    pass
-
-
-@given("사용자는 name 을 입력합니다.", target_fixture="member_info")
-def initialized_member_info():
-    member_info = {
-        "name": "test1"
-    }
-    return member_info
-
-
-@when("입력된 name과 함께 사용자 entity를 생성합니다.", target_fixture="member_entity")
-def create_member_entity(member_info):
-    return MemberEntity.new(name=member_info['name'])
-
-
-@then("생성된 entity는 24자리의 고유 id를 가집니다.")
-def verifyed_member_entity_has_a_identity(member_entity):
-    assert len(member_entity.member_id) == 24
+# # from pytest_bdd import given, when, then, scenario
+# from behave import given, when, then, use_step_matcher
+# from src.member import MemberEntity
+#
+#
+# # @scenario('../features/member/member_create.feature', "사용자는 계정을 만들 수 있습니다.")
+# # def test_create_member():
+# #     pass
+#
+#
+# @given("user able input name", target_fixture="member_info")
+# def _():
+#     member_info = {
+#         "name": "test1"
+#     }
+#     return member_info
+#
+#
+# @when("입력된 name과 함께 사용자 entity를 생성합니다.", target_fixture="member_entity")
+# def _(member_info):
+#     return MemberEntity.new(name=member_info['name'])
+#
+#
+# @then("생성된 entity는 24자리의 고유 id를 가집니다.")
+# def _(member_entity):
+#     assert len(member_entity.member_id) == 24
